3653e5f8f00d87f50695c218fffa00d1cc19401f
jpbywater <jpbywater@gmail.com>
2021-06-24 14:53:42 -0400

    moved all start message settings to nlp.py

--- chat/nlp.py
+++ chat/nlp.py
@@ -1,9 +1,12 @@
-# Don't rename first_str() and to_user()
+# Don't rename start_chat_msg() and to_user()
 # If output_from = "Student" message will be sent directly to student (unless supervisor settings change this)
 # If output_from = "Helpee" message will be sent to supervisor (unless no supervisor or supervisor settings change this)
 
-def first_str():
-    return "Hi! I've been given this question but I'm not sure how to answer it. Can you help me?"
+def start_chat_msg():
+    first_text = "Hello! I've been given this question but I'm not sure how to answer it. Can you help me?"
+    first_img = {'ol': '5', 'ow': '4', 'oh': '3', 'sf': '1'}
+    return {'from': 'Student', 'text': first_text, 'image': first_img}
+
 
 def to_user(new_text, new_image, all_chat_data):
     # for testing....
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import user_passes_test
-from chat.nlp import first_str
+from chat.nlp import start_chat_msg
 from chat.models import SavedChat
 from django.contrib.auth.models import User
 from django.contrib.sessions.models import Session
@@ -12,10 +12,6 @@ def is_participant(user):
 
 def is_oz(user):
     return user.groups.filter(name='oz').exists()
-
-
-def start_chat_msg():
-    return {'from': 'Student', 'text': first_str(), 'image': ''}
 
 
 # General views
@@ -42,7 +38,7 @@ def start(request):
 
 @user_passes_test(is_participant)
 def interface(request):
-    return render(request, 'chat/interface.html', {'messages': [start_chat_msg()]})
+    return render(request, 'chat/interface.html', {'start_chat_msg': start_chat_msg()})
 
 
 # Oz views
@@ -73,7 +69,7 @@ def ozinterface(request):
         return redirect('chat/ozstart.html')
     else:
         return render(request, 'chat/ozinterface.html',
-                      {'messages': [start_chat_msg()],
+                      {'start_chat_msg': start_chat_msg(),
                        'participant_name': request.session['ws_group_name'],
                        'supervisor_role': request.session['supervisor_role']
                        }
